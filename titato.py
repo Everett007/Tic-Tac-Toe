f82f5b6db7603e557b23c9434a312b83ff71ba6f
Everett007 <EverettDW13@gmail.com>
2015-10-03 16:10:15 -0400

    Development Update #1

--- titato.py
+++ titato.py
@@ -1,6 +1,28 @@
 # Tic Tac Toe
-def pBoard():
-   print 'HELLO WORLD'
-  
-print 'This is amazing!'
+# Importing important stuff
+import os
+import random
 
+# Defining global variables
+board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+p1 = 'X'
+p2 = 'O'
+
+# Function to clear screen
+def cls():
+  os.system('cls' if os.name == 'nt' else 'clear')
+
+# Function to print the board
+def pB():
+  cls()
+  print board[0] + ' | ' + board[1] + ' | ' + board[2]
+  print '  |   |  '
+  print '---------'
+  print '  |   |  '
+  print board[3] + ' | ' + board[4] + ' | ' + board[5]
+  print '  |   |  '
+  print '---------'
+  print '  |   |  '
+  print board[6] + ' | ' + board[7] + ' | ' + board[8]
+
+pB()
